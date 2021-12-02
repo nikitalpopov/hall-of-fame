361a649024b2a3ba6ee43474fc88147ea4215d0f
Nikita Popov <mailto.nikitapopov@gmail.com>
2021-12-02 18:46:31 +0100

    Add playlists

--- scripts/generatePlaylistId.js
+++ scripts/generatePlaylistId.js
@@ -1,9 +1,12 @@
 function generatePlaylistId() {
   const playlists = [
-    '6YPylUVqvGdjZCHZjx1eRK',
-    '1ya1ESCp6Ddwmd7vGUbg6n',
-    '1YaK3jHu4gAw7IsjKAqWMn',
-    '5yYav5SK65hKRRC6iJxi3C'
+    '6YPylUVqvGdjZCHZjx1eRK', // electric
+    '1ya1ESCp6Ddwmd7vGUbg6n', // matryoshka
+    '1YaK3jHu4gAw7IsjKAqWMn', // groovy tunes
+    '5yYav5SK65hKRRC6iJxi3C', // tiger
+    '76u87oiboPDaxEEWuuNlZn', // soviet
+    '6P0FOkACTylNaub7NnrOOJ', // loopback
+    '0ojaJFdHtsa0utaaYtqWZI', // грррлс
   ];
 
   if (window.location.pathname.includes("/playlist")) {
